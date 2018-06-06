a339dbdfa39f6c0a3797d3c281fb84dba307a09d
petterssonviktoria <33024880+petterssonviktoria@users.noreply.github.com>
2018-06-06 23:15:31 +0200

    d

--- header.php
+++ header.php
@@ -11,8 +11,19 @@
 	</head>
 	<body>
 		<header>
-			<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 
-			'menu_class' => 'primary-menu', ) ); ?>
+			
+			
+			<div id="menu">
+		      <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+		    </div>
+		    <h1>
+		      <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php bloginfo('name'); ?></a>
+		    </h1>
+		    <p>
+		      <?php bloginfo( 'description' ); ?>
+		    </p>
+    
+
 		</header>
 		
 	</body>
