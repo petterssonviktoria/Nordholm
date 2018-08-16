eef61a23984f912e0cc789da38d290cdcfbf7f65
petterssonviktoria <33024880+petterssonviktoria@users.noreply.github.com>
2018-08-16 21:01:04 +0200

    s

--- front-page.php
+++ front-page.php
@@ -37,14 +37,15 @@ if ( $the_query->have_posts() ) {
 		$the_query->the_post();
 		echo '<div class="fram-del">';
 			
-			echo '<div class="post-over">';
-				the_content();
-			echo '</div>';
 			echo '<div class="post-over post-title">';
 				echo '<h2>';
 					the_title();
 				echo '</h2>';
 			echo '</div>';
+			echo '<div class="post-over">';
+				the_content();
+			echo '</div>';
+
 		echo '</div>';
 	}
 	echo '</ul>';
