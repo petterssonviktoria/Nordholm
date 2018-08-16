979ead4df03f7e426163481815d79d715a99cf40
petterssonviktoria <33024880+petterssonviktoria@users.noreply.github.com>
2018-08-16 21:30:55 +0200

    h

--- front-page.php
+++ front-page.php
@@ -55,5 +55,46 @@ if ( $the_query->have_posts() ) {
 	// no posts found
 }
 
+
+
+
+	/* Template name: om*/
+	get_header();
+
+
+
+
+
+
+$args = array(
+	'post_type' => 'om', 'posts_per_page'=>-1,
+	
+);
+
+// The Query
+$the_query = new WP_Query( $args );
+
+// The Loop
+if ( $the_query->have_posts() ) {
+	echo '<ul class="om">';
+	while ( $the_query->have_posts() ) {
+		$the_query->the_post();
+		echo '<div class="post-over post-title">';
+			echo '<h2>';
+				the_title();
+			echo '</h2>';
+		echo '</div>';
+		echo '<div class="post-over">';
+			the_content();
+		echo '</div>';
+	}
+	echo '</ul>';
+	/* Restore original Post Data */
+	wp_reset_postdata();
+} else {
+	// no posts found
+}
+
+
 	get_footer();
 ?>
--- functions.php
+++ functions.php
@@ -295,6 +295,46 @@ function nordholm_galleri() {
 
 add_action( 'init', 'nordholm_galleri', 0 );
 
+
+function nordholm_om() {
+
+	$labels = array(
+		'name'                  => _x( 'Ome', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Om', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Om', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+	);
+	$rewrite = array(
+		'slug'                  => 'om',
+		'with_front'            => true,
+		'pages'                 => true,
+		'feeds'                 => true,
+	);
+	$args = array(
+		'label'                 => __( 'Om', 'text_domain' ),
+		'description'           => __( 'Post Type Description', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'rewrite'               => $rewrite,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'om', $args );
+
+}
+
+add_action( 'init', 'nordholm_om', 0 );
+
 //Footer widgets
 function nordholm_widgets_init() {
 	register_sidebar( array(
